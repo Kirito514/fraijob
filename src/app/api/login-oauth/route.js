e668400e0cdebbba76fab894c2b25017e44469d2
Kirito514 <student02949@gmail.com>
2025-07-25 00:49:56 +0500

    yangi o'zgarishlar

--- src/app/api/login-oauth/route.js
+++ src/app/api/login-oauth/route.js
@@ -1,8 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 import jwt from 'jsonwebtoken';
 import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
 
 export async function POST(request) {
   try {
--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,9 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from '@/lib/prisma';
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function POST(request) {
   try {
